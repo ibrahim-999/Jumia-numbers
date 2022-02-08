86218dcb08f7266a49436507fbc43bdc8b3be476
ibrahim-999 <ibrahimkhalaf99@gmail.com>
2022-02-08 09:43:44 +0200

    [DEV]: customer-model

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -8,5 +8,21 @@ use Illuminate\Database\Eloquent\Builder;
 
 class Customer extends Model
 {
+    protected $table = "customer";
 
+
+    /***************** scopes ******************** */
+
+    /**
+     * Scope a query to get customers by specific country.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeCountry($query, ?string $country):Builder
+    {
+        $code = array_search($country, Country::Countries);
+
+        return filled($country) ? $query->where('phone', 'LIKE', '(' . $code. ')%') : $query;
+    }
 }
