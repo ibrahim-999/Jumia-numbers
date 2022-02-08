1e3d5c26de3fbb9cf5b0dc7f0e302e72b60251ec
ibrahim-999 <ibrahimkhalaf99@gmail.com>
2022-02-08 13:57:03 +0200

    [DEV]: customer-controller

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -21,10 +21,5 @@ class CustomersController extends Controller
      *
      * @return json
      */
-    public function index(FilterCustomer $request)
-    {
-        $customers = $this->customerService->index($request->country, $request->state);
 
-        return response()->json($customers);
-    }
 }
