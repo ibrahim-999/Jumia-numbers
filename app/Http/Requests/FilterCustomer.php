4bc942d1b6c9cc1a622292e4b7adedecc81c90c2
ibrahim-999 <ibrahimkhalaf99@gmail.com>
2022-02-08 09:44:47 +0200

    [DEV]: customer-request

--- app/Http/Requests/FilterCustomer.php
+++ app/Http/Requests/FilterCustomer.php
@@ -8,5 +8,16 @@ use Pearl\RequestValidate\RequestAbstract;
 
 class FilterCustomer extends RequestAbstract
 {
-
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'country'  => ["filled", Rule::in(array_values(Country::Countries))],
+            'state'    => 'filled|boolean',
+        ];
+    }
 }
